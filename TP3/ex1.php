a4623350f1441e028249d7117d7c7777ed9155f2
ademyazidi-bit <adem.yazidi@isgb.ucar.tn>
2026-03-06 09:25:42 +0400

    3/6/2026

--- TP3/ex1.php
+++ TP3/ex1.php
@@ -1,18 +1,33 @@
 <?php
-    function securiserMdp(string $mdps){
-      $hash =  mdps.trim();
-      return $hash;
-    } 
-    function authentifier(string $saisie, string $hash){
-        if ($saisie == $hash) {
-            return true;    
-        
-    }
-        else {
-            return false;}}
-    $mdps = "password123";
-    securiserMdp($mdps);
-    
+function securiserMdp(string $mdps){
+    $mdps = trim($mdps);
+    return password_hash($mdps, PASSWORD_DEFAULT);
+}
 
+function authentifier(string $saisie, string $hash){
+    $saisie = trim($saisie);
+    return password_verify($saisie, $hash);
+}
 
+$hashStocke = securiserMdp("password123"); // simulé comme si c'était dans la base
+//$saisie = "password123";
+//$hash = securiserMdp($saisie);
+
+//echo "hash genere : ".$hash."<br>";
+//
+//$res = authentifier($saisie, $hash);
+
+//if($res){
+ //   echo "Authentification réussie";
+//}else{
+ //   echo "Authentification échouée";
+//}
+if(isset($_POST["pw"])){
+    $saisiePw = $_POST["pw"];
+
+    if(authentifier($saisiePw, $hashStocke))
+        echo "<h1>Bienvenue dans le site</h1>";
+    else
+        echo "<h2>Erreur de saisie</h2>";
+}
 ?>
